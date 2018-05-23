1e263fba834ee0ba0c00509b83d7497239e474d7
Inderpreet Singh <inderpreet99@gmail.com>
2018-05-22 23:20:34 -0400

    Add logging.

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ SUFFIX=$(date +%Y-%m-%d-%H%M%S)
 grep -q -F 'BASH_IT_CUSTOM' ~/.bash_profile ~/.bashrc
 
 if [[ "$?" != "0" ]]; then
+    echo "Fixing ~/.bash_profile with BASH_IT_CUSTOM."
     sed -i."$SUFFIX" '/^export BASH_IT=/ s#$#\
 export BASH_IT_CUSTOM="'"$BASEDIR"'"#' ~/.bash_profile
+else
+    echo "BASH_IT_CUSTOM already exists in ~/.bash_profile"
 fi
