d11d9d64de340e48502db35cf44aa3fa9dbadb7a
Inderpreet Singh <inderpreet99@gmail.com>
2018-05-26 20:00:21 -0400

    Add Bash completion

--- plugins/general.bash
+++ plugins/general.bash
@@ -1,5 +1,4 @@
 
-CLICOLOR=1
 PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH:/usr/local/mysql/bin
 
 ff() { find . -name "$@" -print; }
@@ -22,7 +21,16 @@ function sedeasy {
 	sed -i "s/$(echo $1 | sed -e 's/\([[\/.*]\|\]\)/\\&/g')/$(echo $2 | sed -e 's/[\/&]/\\&/g')/g" $3
 }
 
-export CLICOLOR
+# bash completion
+if [ -f $(brew --prefix)/etc/bash_completion ]; then
+	. $(brew --prefix)/etc/bash_completion
+fi
+
+
+export CLICOLOR=1
+export LSCOLORS=ExFxCxDxBxegedabagacad
+
 export VISUAL=/usr/bin/vim
 export SVN_EDITOR=/usr/bin/vim
+
 export suffix=$(date +%Y-%m-%d-%H%M%S)
