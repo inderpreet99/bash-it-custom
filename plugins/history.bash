14cf5f025a83febf71abca3644f9499f46370610
Inderpreet Singh <isingh@Iqbals-MacBook-Pro.local>
2018-11-06 20:55:01 -0500

    Preserve bash history without breaking bash-it

--- plugins/history.bash
+++ plugins/history.bash
@@ -4,7 +4,9 @@ export HISTCONTROL=ignoreboth:erasedups
 export HISTFILESIZE=1000000
 export HISTSIZE=1000000
 export HISTTIMEFORMAT='%F %T '
-export PROMPT_COMMAND='history -a'
 
-shopt -s histappend
-shopt -s cmdhist
+# Save and reload the history after each command finishes from https://unix.stackexchange.com/questions/1288/preserve-bash-history-in-multiple-terminal-windows
+export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
+
+shopt -s histappend # history list is appended when the shell exits rather than overwriting the file.
+shopt -s cmdhist # save all lines of a multiple-line command in the same history entry
