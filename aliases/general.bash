e25d5f4692df0fa8b937b081fe351a505c6d99f1
Inderpreet Singh <inderpreet.singh@libertymutual.com>
2018-11-09 15:47:14 -0500

    Add alternating workdisplay config

--- aliases/general.bash
+++ aliases/general.bash
@@ -9,6 +9,7 @@ alias j='jobs -l'
 alias p='~/bin/pause'
 
 alias workdisplay="displayplacer.dms 'id:69733378 res:1920x1200 scaling:off origin:(0,0) degree:0' 'id:1007459267 res:1920x1200x60 scaling:off origin:(1920,-1200) degree:0' 'id:1007459268 res:1920x1200x60 scaling:off origin:(0,-1200) degree:0'"
+alias workdisplay2="displayplacer.dms 'id:69733378 res:1920x1200 scaling:off origin:(0,0) degree:0' 'id:1007459268 res:1920x1200x60 scaling:off origin:(1920,-1200) degree:0' 'id:1007459267 res:1920x1200x60 scaling:off origin:(0,-1200) degree:0'"
 
 [ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
 
