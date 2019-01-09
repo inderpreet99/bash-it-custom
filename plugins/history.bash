ce373ac62d4313f7a967448eada7220f845a8b85
Inderpreet Singh <inderpreet.singh@libertymutual.com>
2019-01-09 11:55:21 -0500

    Stop having one history for all terminals.
    Previously, we had problems with pressing Up arrow
    and getting other terminal's commands.

--- plugins/history.bash
+++ plugins/history.bash
@@ -5,8 +5,5 @@ export HISTFILESIZE=1000000
 export HISTSIZE=1000000
 export HISTTIMEFORMAT='%F %T '
 
-# Save and reload the history after each command finishes from https://unix.stackexchange.com/questions/1288/preserve-bash-history-in-multiple-terminal-windows
-export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
-
 shopt -s histappend # history list is appended when the shell exits rather than overwriting the file.
 shopt -s cmdhist # save all lines of a multiple-line command in the same history entry
