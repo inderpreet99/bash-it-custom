0c0758b2ddd2dae01ada134a6fe08c347697b9f5
Inderpreet Singh <inderpreet99@gmail.com>
2019-01-26 17:42:25 -0500

    Switch to Python 3.7

--- plugins/python.bash
+++ plugins/python.bash
@@ -1,6 +1,6 @@
 
 # PYTHONPATH=$HOME/lib/python:/usr/local/lib/python2.7/site-packages
 # export PYTHONPATH
-PATH=/usr/local/share/python:/usr/local/share/python3:$HOME/Library/Python/2.7/bin/:$PATH
+PATH=/usr/local/share/python:/usr/local/share/python3:$HOME/Library/Python/3.7/bin:$PATH
 
 export DYLD_LIBRARY_PATH=/usr/local/mysql/lib # fixes MySQL-python pip package problem with local MySQL install
