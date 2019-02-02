09a41491b4e812049d99d8ba5747976decd3eb1c
Inderpreet Singh <inderpreet99@gmail.com>
2019-02-01 22:26:41 -0500

    AWS init once

--- installers/aws.sh
+++ installers/aws.sh
@@ -1,2 +1,6 @@
 pip3 install awscli
-aws configure
+
+if [[ ! -f ~/.aws/credentials ]]; then
+    echo "Running AWS configure for first time. Get credentials from AWS Console."
+    aws configure
+fi
