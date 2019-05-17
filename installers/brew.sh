488be2cfd12a9e725304120aad68d28a42d5266d
Inderpreet Singh <inderpreet.singh@libertymutual.com>
2019-05-17 12:41:56 -0400

    Brew updates

--- installers/brew.sh
+++ installers/brew.sh
@@ -1,5 +1,8 @@
 BASEDIR="$( cd "$(dirname "$0")" ; pwd -P )"
+brew update
 brew bundle --file=$BASEDIR/Brewfile
 . "/usr/local/opt/nvm/nvm.sh"
+
+# Node setup after brew installers
 nvm install 10
 nvm use 10
