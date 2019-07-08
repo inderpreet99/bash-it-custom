bc608d4dcf871721c06adbd7b7e03e35bd351650
Inderpreet Singh <isingh2@wayfair.com>
2019-07-08 09:58:16 -0400

    Reshuffle brew file to fix node

--- installers/brew.sh
+++ installers/brew.sh
@@ -1,8 +1,3 @@
 BASEDIR="$( cd "$(dirname "$0")" ; pwd -P )"
 brew update
 brew bundle --file=$BASEDIR/Brewfile
-. "/usr/local/opt/nvm/nvm.sh"
-
-# Node setup after brew installers
-nvm install 10
-nvm use 10
