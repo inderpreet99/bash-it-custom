97155012a9252b22ab93e9c135a1cfb98bbcf66e
Inderpreet Singh <isingh2@wayfair.com>
2019-07-08 09:58:32 -0400

    Split pagekite from mac setup

---
+++ installers/lm/pagekite.sh
@@ -0,0 +1,6 @@
+
+if [[ ! -f ~/bin/pagekite.py ]]; then
+    mkdir -p ~/bin/
+    curl -o ~/bin/pagekite.py https://pagekite.net/pk/pagekite.py
+    chmod u+x ~/bin/pagekite.py
+fi
--- installers/mac.sh
+++ installers/mac.sh
@@ -1,11 +1,14 @@
 # Required for installing Xcode tools
-xcode-select --install
+# xcode-select --install
 
 # Show Library folder
 chflags nohidden ~/Library
 
 # Show hidden files
 defaults write com.apple.finder AppleShowAllFiles YES
+
+# Disable press and hold key to allow repeating the same key again
+defaults write -g ApplePressAndHoldEnabled -bool false
 
 # Show path bar
 defaults write com.apple.finder ShowPathbar -bool true
@@ -15,9 +18,3 @@ defaults write com.apple.finder ShowStatusBar -bool true
 
 # fixes VSCode font for Mojave https://github.com/Microsoft/vscode/issues/51132
 defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO
-
-if [[ ! -f ~/bin/pagekite.py ]]; then
-    mkdir -p ~/bin/
-    curl -o ~/bin/pagekite.py https://pagekite.net/pk/pagekite.py
-    chmod u+x ~/bin/pagekite.py
-fi
