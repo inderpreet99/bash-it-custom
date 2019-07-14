cf648e274773f36c6c7a49a2b1727a00ef4185b9
Inderpreet Singh <inderpreet99@gmail.com>
2019-07-14 17:11:54 -0400

    Fix node setup as usual

--- installers/nvm.sh
+++ installers/nvm.sh
@@ -3,5 +3,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . "$NVM_DIR/nvm.sh"
 
 # Node setup after brew installers
-nvm use 10      # regular world
 nvm use 8.9.4   # wayfair
+nvm use stable
+nvm alias default stable
--- plugins/node.bash
+++ plugins/node.bash
@@ -1,4 +1,2 @@
 export NVM_DIR="$HOME/.nvm"
 . "/usr/local/opt/nvm/nvm.sh"
-
-function npm-do { (PATH=$(npm bin):$PATH; eval $@;) }
